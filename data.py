1131e21089e6344a30a6f3242856f8e0bdf00680
Alabi Michael Ayomide <michaelalabi1601@gmail.com>
2026-05-15 18:01:16 +0100

    Tuning the Regressor Base Model
    I decided to use Random Forest Hyperparameter Tuning using GridSearchCV because Instead of running Trial A and Trial B manually, GridSearchCV creates a "grid" of all possible combinations of settings (hyperparameters) you want to test. It then runs them all and tells you exactly which one resulted in the lowest error (RMSE).
    
    I also decided to add a code that shows the amount of missing values per column as part of my Pre-processing validation.
    
    I learnt how to check the number of rows and columns (shapes) of the already split dataset, so I put it there as well.
    
    I put well detailed comments to explain my hyperparameters and the param_grid I created

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt# we will use this library to show a plot of Actual vs Prediction.
 
 df = pd.read_csv('robot_inverse_kinematics_dataset.csv')#This is the dataset we will be using for our project. It contains the joint angles and corresponding end-effector positions for a robotic arm.
+print(df.isnull().sum()) # This is to check for any missing values in the dataset. It will return the count of missing values for each column.
 print(df.head(10)) # For the data preview
 
 
@@ -19,6 +20,12 @@ X = df[['q1','q2','q3','q4','q5','q6']] #Our input features; joint angles (q1 to
 y = df[['x','y','z']] #Our output variables; end-effector positions (x,y,z).y is lowercase to show it's a vector.
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)#We split the dataset into training and testing sets, with 20% of the data reserved for testing. The random_state parameter ensures that the split is reproducible.
+
+#Data shapes : We print the shapes of the training and testing sets to confirm that they have been split correctly. The shape will show the number of samples and features in each set.
+print(f'X_train : {X_train.shape}')
+print(f'X_test : {X_test.shape}')
+print(f'y_train : {y_train.shape}')
+print(f'y_test : {y_test.shape}')
 
 #Model training (Base Model)
 print("---Training Base Model---")
@@ -39,3 +46,34 @@ print(f"Mean Squared Error (MSE): {mse:.6f}") #We print the MSE with 6 decimal p
 print(f"Mean Absolute Error (MAE): {mae:.6f}") #We print the MAE with 6 decimal places.
 print(f"R^2 Score: {r2:.6f}") #We print the R^2 Score with 6 decimal places.
 print(f"Root Mean Squared Error (RMSE): {rmse:.6f}") #We print the RMSE with 6 decimal places.      
+
+# I now want to use Random Forest Hyperparameter Tuning using GridSearchCV to tune my Regressor base model.
+from sklearn.model_selection import GridSearchCV
+ # Define the grid of hyperparameters to search for testing
+param_grid = {
+    'n_estimators': [50, 100, 150], # Number of trees in the forest
+    'max_depth': [None, 10, 20], # Maximum depth of the tree. a None depth allows the trees to grow fully 
+    'max_features':['sqrt','log2']# Number of features to consider at each split.
+    }
+
+#Initialize the Grid Search
+grid_search = GridSearchCV(estimator=rf_base, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2, scoring='r2') #We initialize the GridSearchCV with our base model (rf_base), the parameter grid (param_grid), 5-fold cross-validation (cv=5), and parallel processing (n_jobs=-1) to speed up the search. The verbose parameter is set to 2 to print detailed logs of the search process.
+
+#Run the search
+print("---Starting Hyperpaarameter Tuning---")
+grid_search.fit(X_train, y_train)# this is fitting the hyperparameter tuning model to the training data.
+ # The best result 
+print(f"Best Hyperparameters: {grid_search.best_params_}") #We print the best hyperparameters found by the grid search.
+
+#Evaluate the best model
+best_model = grid_search.best_estimator_ #We get the best model from the grid search.
+y_pred_best = best_model.predict(X_test) #We use the best model to make predictions on the test set.
+mse_best = mean_squared_error(y_test, y_pred_best) #We calculate the MSE for the best model.
+mae_best = mean_absolute_error(y_test, y_pred_best) #We calculate the MAE for the best model.
+r2_best = r2_score(y_test, y_pred_best) #We calculate the R^2 Score for the best model.
+rmse_best = np.sqrt(mse_best) #We calculate the RMSE for the best model.
+#Results (Best Model)
+print(f"Best Model Mean Squared Error (MSE): {mse_best:.6f}") # the MSE for the best model .
+print(f"Best Model Mean Absolute Error (MAE): {mae_best:.6f}") # the MAE for the best model .
+print(f"Best Model R^2 Score: {r2_best:.6f}") #the R^2 Score for the best model.
+print(f"Best Model Root Mean Squared Error (RMSE): {rmse_best:.6f}") # the RMSE for the best model.
